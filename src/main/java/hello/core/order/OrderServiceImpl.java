e0ddce57b542ee0ddf9dc7625bcd1c3e773f885c
gltdhd <gltdhd@gmail.com>
2023-10-17 19:40:43 +0900

    23/10/17 스프링 핵심 원리 이해1 - 예제 생성

---
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -0,0 +1,21 @@
+package hello.core.order;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.Member;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+
+public class OrderServiceImpl implements OrderService{
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+        //회원정보 조회
+        Member member = memberRepository.findById(memberId);
+        //할인 적용
+        int discountPrice = discountPolicy.discount(member, itemPrice);
+
+        return new Order(memberId,itemName,itemPrice,discountPrice);
+    }
+}
