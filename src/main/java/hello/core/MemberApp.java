31201f68c832e278746ce7a40aac978945d1aa8e
gltdhd <gltdhd@gmail.com>
2023-10-19 17:14:01 +0900

    23/10/19 스프링 컨테이너와 스프링 빈

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -11,7 +11,10 @@ public class MemberApp {
     public static void main(String[] args) {
 //        AppConfig appConfig = new AppConfig();
 //        MemberService memberService = appConfig.memberService();
-        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        // 스프링 컨테이너 생성
+        ApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+
         MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
 
         Member member = new Member(1L, "memberA", Grade.VIP);
